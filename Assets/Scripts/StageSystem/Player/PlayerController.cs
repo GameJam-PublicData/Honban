3dbc7381724f875ba64dc324792acec8727ae00c
yutoi <yuuto_24s1101205@nnn.ed.jp>
2026-03-17 20:36:32 +0900

    RigidbodyをRigidbody2Dに変換

--- Assets/Scripts/StageSystem/Player/PlayerController.cs
+++ Assets/Scripts/StageSystem/Player/PlayerController.cs
@@ -1,37 +1,39 @@
-using System;
+using InputSystemActions;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using InputSystemActions;
 
-public class PlayerController : MonoBehaviour
+namespace StageSystem.Player
 {
-    InputActions _inputActions;
-    InputAction _moveAction;
+    public class PlayerController : MonoBehaviour
+    {
+        InputActions _inputActions;
+        InputAction _moveAction;
     
-    [SerializeField] float moveSpeed;
-    private Rigidbody _rb;
+        [SerializeField] float moveSpeed;
+        private Rigidbody2D _rb;
 
 
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody>();
-        _inputActions = new InputActions();
-        _inputActions.Player.Enable();
-        _moveAction = _inputActions.Player.Move;
-    }
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _inputActions = new InputActions();
+            _inputActions.Player.Enable();
+            _moveAction = _inputActions.Player.Move;
+        }
 
-    void Update()
-    {
-        Vector3 moveValue = _moveAction.ReadValue<Vector2>();
-        moveValue = new Vector3(moveValue.x, 0, moveValue.y);
+        void Update()
+        {
+            Vector3 moveValue = _moveAction.ReadValue<Vector2>();
+            moveValue = new Vector3(moveValue.x, 0, moveValue.y);
 
 
-        Vector3 moveDirection = transform.TransformDirection(moveValue);
-        _rb.MovePosition(_rb.position + moveDirection * (moveSpeed * Time.deltaTime));
-    }
+            Vector3 moveDirection = transform.TransformDirection(moveValue);
+            _rb.MovePosition(transform.position + moveDirection * (moveSpeed * Time.deltaTime));
+        }
 
-    private void OnDestroy()
-    {
-        _inputActions.Player.Disable();
+        private void OnDestroy()
+        {
+            _inputActions.Player.Disable();
+        }
     }
 }
