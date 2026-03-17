9a208c354de12047f705769fc67e67fc33d99a63
MacG <kemutan0909@gmail.com>
2026-03-17 20:21:25 +0900

    中身ないです

---
+++ Assets/Scripts/StageSystem/Ink/InkAntiGravity.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace StageSystem.Ink
+{
+public class InkAntiGravity : MonoBehaviour,IInkEffect
+{
+    public void UpdateInkArea(Rigidbody rigidbody)
+    {
+        
+    }
+
+    public void StartInkArea(Rigidbody rigidbody)
+    {
+        
+    }
+
+    public void StopInkArea(Rigidbody rigidbody)
+    {
+        throw new NotImplementedException();
+    }
+}
+}
