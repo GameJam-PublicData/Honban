41cb3d9da3b6350bbc7129e3780bb270b3b3666f
Axzin <Pchan0961@gmail.com>
2026-03-17 14:49:52 +0900

    2D化

---
+++ Assets/Scripts/StageSystem/InkArea.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StageSystem
+{
+public interface IInkArea
+{
+    void  CreateInkArea(List<Vector2> points, IInkEffect effect);
+}
+/// <summary> インクエリアのオブジェクトにつけられる </summary>
+public class InkArea : MonoBehaviour, IInkArea
+{
+    [SerializeField] MeshFilter meshFilter;
+    Mesh _mesh;
+
+    void Reset()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    public void CreateInkArea(List<Vector2> points, IInkEffect effect)
+    {
+        _mesh = new Mesh();
+        //pointsに沿った形のMeshを作る
+        
+    }
+}
+}
+  
+  
