fca1845f63bc846439335ea123625c05ec6518fa
ta-ku44 <213548904+ta-ku44@users.noreply.github.com>
2026-03-18 23:49:25 +0900

    feat: 背景切り替えをマテリアル差し替えからシェーダーカラーのDOTweenアニメーションに変更

--- Assets/Scripts/StageSystem/BackgroundChanger.cs
+++ Assets/Scripts/StageSystem/BackgroundChanger.cs
@@ -1,29 +1,37 @@
 using System;
 using UnityEngine;
 using R3;
+using DG.Tweening;
 
 namespace StageSystem
 {
 public class BackgroundChanger : MonoBehaviour
 {
+    static readonly int ColorUp = Shader.PropertyToID("_Color_Up");
+    static readonly int ColorDown = Shader.PropertyToID("_Color_Down");
+
     [Serializable]
     struct BackGroundData
     {
         public float SwitchYPos;
-        public Material BackgroundMaterial;
+        public Color Up;
+        public Color Down;
     }
     [SerializeField] BackGroundData[] backGroundDataArray;
     [SerializeField] GameObject playerObj;
-    
-    SpriteRenderer _spriteRenderer;
+    [SerializeField] float duration = 0.5f;
+
+    Material _mat;
 
     void Init()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        var spriteRenderer = GetComponent<SpriteRenderer>();
         
-        if (_spriteRenderer == null) throw new Exception("SpriteRendererが見つかりませんでした。");
+        if (spriteRenderer == null) throw new Exception("SpriteRendererが見つかりませんでした。");
         if (playerObj == null) throw new Exception("playerObjが設定されていません。");
         if (backGroundDataArray == null || backGroundDataArray.Length == 0) throw new Exception("backGroundDataArrayが設定されていません。");
+        
+        _mat = spriteRenderer.material;
     }
 
     void Start()
@@ -56,6 +64,12 @@ public class BackgroundChanger : MonoBehaviour
         return result;
     }
 
-    void ChangeBackground(int index) => _spriteRenderer.material = backGroundDataArray[index].BackgroundMaterial;
+    void ChangeBackground(int index)
+    {
+        var data = backGroundDataArray[index];
+    
+        DOTween.To(() => _mat.GetColor(ColorUp), c => _mat.SetColor(ColorUp, c), data.Up, duration);
+        DOTween.To(() => _mat.GetColor(ColorDown), c => _mat.SetColor(ColorDown, c), data.Down, duration);
+    }
 }
 }
