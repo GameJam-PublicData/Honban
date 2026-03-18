1493e1f177b381cc2af1cdc0ffa770aa144979fa
Axzin <Pchan0961@gmail.com>
2026-03-18 16:40:05 +0900

    チェックポイント処理

--- Assets/Scripts/StageSystem/CheckPoint/CheckPoint.cs
+++ Assets/Scripts/StageSystem/CheckPoint/CheckPoint.cs
@@ -1,11 +1,30 @@
+using System;
+using UnityEngine;
+
 namespace StageSystem.CheckPoint
 {
 public interface ICheckPoint
 {
-    
+    public void Initialize(ICheckPointManager checkPointManager);
+    Transform Transform { get; }
 }
-public class CheckPoint
+public class CheckPoint :MonoBehaviour,ICheckPoint
 {
-    
+    ICheckPointManager _checkPointManager;
+    public Transform Transform => transform;
+    void OnTriggerEnter2D(Collider2D other)
+    { 
+        if (other.CompareTag("Player"))
+        {
+            _checkPointManager.ThroughCheckPoint(this);
+        }
+        
+    }
+
+    public void Initialize(ICheckPointManager checkPointManager)
+    {
+        Debug.Log($"Initializing checkpoint: {transform.name}");
+        _checkPointManager = checkPointManager;
+    }
 }
 }
--- Assets/Scripts/StageSystem/CheckPoint/CheckPointManager.cs
+++ Assets/Scripts/StageSystem/CheckPoint/CheckPointManager.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 namespace StageSystem.CheckPoint
 {
-public class CheckPointManager  : MonoBehaviour
+public interface ICheckPointManager
+{
+    public ICheckPoint CurrentCheckPoint { get; }
+    public void ThroughCheckPoint(ICheckPoint checkPoint);
+    public void MoveCheckPoint(Transform transform);
+    
+}
+public class CheckPointManager  : MonoBehaviour, ICheckPointManager
 {
       List<ICheckPoint>  _checkPoints = new();
 
-      public ICheckPoint CheckPoint;
+      public ICheckPoint CurrentCheckPoint { get; private set; }
+      
+      
 
       void Reset()
       {
-          _checkPoints.Clear();
-          transform.GetChild(0).GetComponentsInChildren(true, _checkPoints);
+
       }
 
-      void Start()
+      void Awake()
       {
+          _checkPoints.Clear();
+          transform.GetChild(0).GetComponentsInChildren(true, _checkPoints);
+          Debug.Log($"CheckPointManager: Found {_checkPoints.Count} checkpoints in children.");
           _checkPoints.ForEach(checkPoint =>
           {
-              
+              checkPoint.Initialize(this);
           });
+      }
+      
+
+      public void ThroughCheckPoint(ICheckPoint checkPoint)
+      {
+          Debug.Log($"Through checkpoint: {checkPoint.Transform.name}");
+          CurrentCheckPoint = checkPoint;
+      }
+
+      public void MoveCheckPoint(Transform transform)
+      {
+          transform.position = CurrentCheckPoint.Transform.position;
       }
 }
 }
