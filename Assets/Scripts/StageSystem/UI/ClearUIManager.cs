de8ad5abae899001b25d33ae194d10257c942aed
Axzin <Pchan0961@gmail.com>
2026-03-19 13:42:55 +0900

    クリアUI修正

--- Assets/Scripts/StageSystem/UI/ClearUIManager.cs
+++ Assets/Scripts/StageSystem/UI/ClearUIManager.cs
@@ -1,4 +1,5 @@
 using System;
+using MainSystem.Scene;
 using StageSystem.Player;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,13 +17,31 @@ public class ClearUIManager : MonoBehaviour , IClearUIManager
     [SerializeField] Image clearBackground;
     [SerializeField] Image gameOverImage;
 
+    [SerializeField] Button MenuButton;
+    ISceneLoader _sceneLoader;
+    
+    
     IActiveHandler _activeHandler;
     [Inject]
-    public void Construct(IActiveHandler activeHandler)
+    public void Construct(IActiveHandler activeHandler,ISceneLoader  sceneLoader)
     {
         _activeHandler = activeHandler;
+        _sceneLoader = sceneLoader;
     }
-    
+
+    void Awake()
+    {
+        MenuButton.onClick.AddListener(() =>
+        {
+            _sceneLoader.LoadScene(SceneType.MainMenuScene);
+        });
+    }
+
+    void OnDestroy()
+    {
+        MenuButton.onClick.RemoveAllListeners();
+    }
+
     public void Initialize(bool isClear)
     {
         Debug.Log("クリアUI表示");
@@ -32,11 +51,13 @@ public class ClearUIManager : MonoBehaviour , IClearUIManager
             clearImage.gameObject.SetActive(true);
             clearBackground.gameObject.SetActive(true);
             gameOverImage.gameObject.SetActive(false);
+            MenuButton.gameObject.SetActive(false);
         }
         else
         {
             clearImage.gameObject.SetActive(false);
             gameOverImage.gameObject.SetActive(true);
+            MenuButton.gameObject.SetActive(true);
         }
         _activeHandler.StopGame();
     }
